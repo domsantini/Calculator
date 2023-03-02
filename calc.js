207f6728b3067df814cb7ab52e215c8ad382d05b
domsantini <111545540+domsantini@users.noreply.github.com>
2023-03-01 18:33:52 -0800

    Slight refractor of variables

--- calc.js
+++ calc.js
@@ -1,4 +1,5 @@
-let equation = document.querySelector('.equation');
+let result = document.querySelector('#result')
+let equation = document.querySelector('#equation');
 const buttons = document.querySelectorAll('button');
 const buttonArray = Array.from(buttons);
 const numButtons = document.querySelector('.num')
@@ -6,18 +7,16 @@ const operators = document.querySelectorAll('.operator')
 const clear = document.querySelector('.clear');
 const del = document.querySelector('.delete');
 
-let displayNum = '';
+let currentNum = '';
 let num1 = '';
 let num2 = '';
 let operator = '';
 
-console.log(operators)
-
 buttonArray.forEach((item) => {
     item.addEventListener('click', e => {
-        displayNum += e.target.innerHTML;
-        equation.innerHTML = displayNum;
-        console.log(`Disp: ${displayNum}, Num1: ${num1}, Num2: ${num2}, Op: ${operator}`);
+        currentNum += e.target.innerHTML;
+        result.innerHTML = currentNum;
+        console.log(`Disp: ${currentNum}, Num1: ${num1}, Num2: ${num2}, Op: ${operator}`);
         
     })
 })
@@ -25,34 +24,34 @@ buttonArray.forEach((item) => {
 operators.forEach((button) => {
     button.addEventListener('click', e => {
         operator = e.target.innerHTML;
-        if (num1) {
-            num2 = num1;
+        if (num2) {
+            num1 = num2;
         }
         
-        num1 = displayNum.slice(0, -1);
-        displayNum = '';
+        num2 = currentNum.slice(0, -1);
+        currentNum = '';
         
-        console.log(`Disp: ${displayNum}, Num1: ${num1}, Num2: ${num2}, Op: ${operator}`);
+        console.log(`Disp: ${currentNum}, Num1: ${num1}, Num2: ${num2}, Op: ${operator}`);
     })
 })
 
 clear.addEventListener('click', () => {
-    num1 = num2 = operator = displayNum = '';
+    num1 = num2 = operator = currentNum = '';
     equation.innerHTML = displayNum;
 })
 
 
 function add() {
-    console.log(a + b)
+    return (a + b)
 }
 function subtract() {
-    console.log(a - b)
+    return (a - b)
 }
 function multiply() {
-    console.log(a * b)
+    return (a * b)
 }
 function divide() {
-    console.log(a / b)
+    return (a / b)
 }
 
 
